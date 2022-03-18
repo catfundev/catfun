a313b403db214256aad37e8fb33b2da7b9fbb488
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-03-18 14:55:06 +0500

    tuning

--- components/section-reviews.tsx
+++ components/section-reviews.tsx
@@ -1,4 +1,3 @@
-import Section from "./section";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {EffectCards, Navigation} from "swiper";
 
@@ -6,17 +5,21 @@ import "swiper/css";
 import "swiper/css/navigation";
 import {styled} from "@mui/material";
 
-const MySection = styled(Section)`
+const MySection = styled("section")`
+  display: flex;
+  margin: auto;
+  padding: 4rem 0;
   min-height: 80vh;
 `
 
 const MySwiper = styled(Swiper)`
+  width: 100%;
+
   .swiper-slide {
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 20px;
-    width: 70vw;
   }
 
   .swiper-slide-shadow {
@@ -24,41 +27,41 @@ const MySwiper = styled(Swiper)`
   }
 
 
-  & > .swiper-wrapper {
-    //  transition: all 1s;
-    //  & > .swiper-slide {
-    //    display: flex;
-    //    align-items: center;
-    //    justify-content: center;
-    //  }
-    //
-    //  & > .swiper-slide-active {
-    //    //text-align: center;
-    //    //z-index: 1;
-    //  }
-    //
-    //  & > .swiper-slide-next {
-    //    transform: translateX(-40em) rotateX(-30deg);
-    //  }
-    //
-    //  & > .swiper-slide-prev {
-    //    transform: translateX(50em);
-    //  }
-    //
-    //  & > .swiper-slide-active {
-    //    //transform: translateX(15%);
-    //    z-index: 1;
-    //
-    //    & > video {
-    //      width: 40em;
-    //    }
-    //  }
-  }
+  //& > .swiper-wrapper {
+  //  transition: all 1s;
+  //  & > .swiper-slide {
+  //    display: flex;
+  //    align-items: center;
+  //    justify-content: center;
+  //  }
+  //
+  //  & > .swiper-slide-active {
+  //    //text-align: center;
+  //    //z-index: 1;
+  //  }
+  //
+  //  & > .swiper-slide-next {
+  //    transform: translateX(-40em) rotateX(-30deg);
+  //  }
+  //
+  //  & > .swiper-slide-prev {
+  //    transform: translateX(50em);
+  //  }
+  //
+  //  & > .swiper-slide-active {
+  //    //transform: translateX(15%);
+  //    z-index: 1;
+  //
+  //    & > video {
+  //      width: 40em;
+  //    }
+  //  }
+  //}
 `
 const MyVideo = styled("video")`
-  width: 100%;
   box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12);
   border-radius: 20px;
+  width: 80%;
 `
 
 function SectionReviews() {
